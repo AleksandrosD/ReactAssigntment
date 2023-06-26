baf8bb2dbbe4c32a7328e6c5847009034dda487f
Aleksandros Doci <aleksandrosdoci@icloud.com>
2023-06-26 19:50:03 -0400

    Search bar added at Nav

--- code/BookForum/src/navbar.jsx
+++ code/BookForum/src/navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar() {
 
     return (
       <>
-       <nav id="header" className=" w-full z-30 top-0 text-white ">
+       <nav id="header" className="w-full z-30 top-0 text-white ">
       <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
         <div className="pl-4 flex items-center">
           <a className="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="#">
@@ -17,12 +17,23 @@ export default function Navbar() {
             </svg>
           </button>
         </div>
-        <div className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20" id="nav-content">
+       
+        <div className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 lg:bg-transparent text-black p-4 lg:p-0 z-20" id="nav-content">
           <ul className="list-reset lg:flex justify-end flex-1 items-center">
-            <li className="mr-3">
+            {/* <li className="mr-3">
               <a className="inline-block py-2 px-4 text-black font-bold no-underline" href="#">Search</a>
             
-            </li>
+            </li> */}
+           <div className="inline-block pr-10">
+          
+          <input className="w-72 py-2 px-4 pl-5 pr-10 rounded-full focus:outline-0 text-black" type="text" placeholder="Search Book..."
+          name="search"></input>
+          <button className="-ml-8 border-6 bg-trasparent" type="submit"><i
+            classname="fa fa-search text-gray-400"></i></button>
+            
+          </div>
+             
+        
             <li className="mr-3">
               <a className="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#">GitHub</a>
             </li>
